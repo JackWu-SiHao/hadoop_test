267a3c695f5e2dba838dd392c857428267b75428
JackWu-SiHao <shwu.cs98@gmail.com>
2014-01-17 13:34:41 +0800

    modify opencl validator

--- opencl/output-validate.py
+++ opencl/output-validate.py
@@ -3,31 +3,20 @@ import sys
 
 # python output-validate.py part1/part2
 
-
-def main():
-    part1_or_2 = sys.argv[1]
-    ANSWER_PATH = "/home/shwu/Dropbox/Shwu/Master/PPC-Hadoop/testcase/{0}.out".format(
-        part1_or_2)
-    STUDENT_ID_PATH = "/home/shwu/Dropbox/Shwu/Master/PPC-Hadoop/testcase/studentID.txt"
-    OUTPUT_PATH = "/home/shwu/hw4-output/"
-    OUTPUT_CORRECT_FILE = "/home/shwu/hw4-outputCorrect/outputCorrectID-{0}".format(
-        part1_or_2)
-    OUTPUT_ERROR_FILE = "/home/shwu/hw4-outputError/outputErrorID-{0}".format(
-        part1_or_2)
-    SCORE_FILE = "/home/shwu/hw4-score/score-{}".format(part1_or_2)
-    # /home/shwu/Dropbox/Shwu/Master/PPC-Hadoop/testcase/studentID.txt
+def validate(answer_file):
+    ANSWER_PATH = "/home/shwu/hw6/" + answer_file
+    SCORE_PATH = "/home/shwu/hw6/score/" + answer_file + ".score"
+    # xxx.out.score
+    OUTPUT_ERROR_FILE = "/home/shwu/hw6/output-error.out"
+    OUTPUT_PATH = "/home/shwu/hw6/output/"
+    file_score = open(SCORE_PATH, "w")
     file_ans = open(ANSWER_PATH, "r")
-    file_id = open(STUDENT_ID_PATH, "r")
+    file_ids = open("/home/shwu/hw6/studentID.txt", "r")
     output_error_file = open(OUTPUT_ERROR_FILE, "w")
-    output_correct_file = open(OUTPUT_CORRECT_FILE, "w")
-    score_file = open(SCORE_FILE, "w")
-
-    number_success = 0
-    number_fail = 0
-
-    ids = [line.strip() for line in file_id]
     ans_lines = [(((line.strip()).replace("\t", "")).replace(" ", ""))
                  for line in file_ans]
+
+    ids = [line.strip() for line in file_id]
     for x in xrange(0, len(ids)):
         OUTPUT_FILE = OUTPUT_PATH + ids[x] + ".out"
         # default is 0
@@ -39,7 +28,7 @@ def main():
             ERROR_MES = "Ouput file not exist"
             SCORE = ids[x] + ": 0 :" + ERROR_MES + "\n"
             # output_error_file.write(ERROR_MES)
-            score_file.write(SCORE)
+            file_score.write(SCORE)
             number_fail += 1
             file_id_ans.close()
             continue
@@ -47,25 +36,22 @@ def main():
         # if len(ans_lines) != len(ans_id_lines):
         #     ERROR_MES = ids[x] + ": " + "Wrong answer(length)\n"
         #     output_error_file.write(ERROR_MES)
-        #     score_file.write(SCORE)
+        #     file_score.write(SCORE)
         #     number_fail += 1
         # else:
         for y in xrange(0, len(ans_id_lines)):
             if ans_id_lines[y] not in ans_lines:
-                print ans_id_lines[y]
+                # print ans_id_lines[y]
                 ERROR_MES = "Wrong answer: " + ans_id_lines[y]
                 SCORE = ids[x] + ": 0 :" + ERROR_MES + "\n"
                 # output_error_file.write(ERROR_MES + "\n")
-                score_file.write(SCORE)
-                number_fail += 1
+                file_score.write(SCORE)
                 file_id_ans.close()
                 break
         # succeed run
         if y == (len(ans_id_lines) - 1):
             SCORE = ids[x] + ": 100\n"
-            number_success += 1
-            output_correct_file.write(ids[x] + "\n")
-            score_file.write(SCORE)
+            file_score.write(SCORE)
         file_id_ans.close()
 
     print "Success:{0}".format(number_success)
@@ -74,7 +60,9 @@ def main():
     file_id.close()
     output_error_file.close()
     output_correct_file.close()
-    score_file.close()
+    file_score.close()
+
+def main():
 
 if __name__ == '__main__':
     main()
--- opencl/runcl.py
+++ opencl/runcl.py
@@ -1,66 +1,70 @@
 import os
 import sys
 import subprocess
+import time
+import itertools
 
 
-def start_action():
-import os
-import subprocess
-import sys
+def start_action(action, input_file):
+    _COMPILE = "compile"
+    _RUN = "run"
 
-def start_action(action):
-	_COMPILE = "compile"
-	_RUN	 = "run"
+    if action == _COMPILE:
+        COMPILE_ERROR_PATH = "/home/shwu/hw6/compile-error.out"
+        file_compile_error = open(COMPILE_ERROR_PATH, "w")
 
-	if action == _COMPILE:
-		# TODO
-		_COMPILE_ERROR_PATH = ""
-		file_compile_error = open(_COMPILE_ERROR_PATH, "w")
+    elif action == _RUN:
+        INPUT_FILE_PATH = "/home/shwu/hw6/" + input_file
+        RUN_ERROR_PATH = "/home/shwu/hw6/run-error.out"
+        RUN_TIME_PATH = "/home/shwu/hw6/run-time.out"
+        file_run_error = open(RUN_ERROR_PATH, "w")
+        file_run_time = open(RUN_TIME_PATH, "w")
 
-	elif action == _RUN:
-		_RUN_ERROR_PATH = ""
-		file_run_error = open(_RUN_ERROR_PATH, "w")
+    else:
+        pass
 
-	else:
-		pass
+    for files in os.listdir("."):
+        files.strip()
+        if os.path.isdir(files):
+            os.chdir(files)
+            if action == _COMPILE:
+                if not subprocess.call(["g++", "-lOpenCL", "histogram.cpp", "-o", "histogram"]):
+                    # correct running
+                    pass
+                else:
+                    _COMPILE_ERROR_MES = files + " compile error\n"
+                    file_compile_error.write(_COMPILE_ERROR_MES)
+            elif action == _RUN:
+                # TODO: add time
+                subprocess.call(["cp", INPUT_FILE_PATH, "input"])
+                tStart = time.time()
+                if not subprocess.call(["./histogram"]):
+                    tEnd = time.time()
+                    dt = (tStart - tEnd)
+                    _RUN_TIME_MES = files + ":" + dt + "\n"
+                    OUTPUT_FILE = files + ".out"
+                    OUTPUT_PATH = "/home/shwu/hw6/output/" + OUTPUT_FILE
+                    file_run_time.write(_RUN_TIME_MES)
+                    subprocess.call(["cp", OUTPUT_FILE, OUTPUT_PATH])
+                else:
+                    _RUN_ERROR_MES = files + "\n"
+                    file_run_error.write(_RUN_ERROR_MES)
+            else:
+                pass
+            os.chdir("..")
 
-	for files in os.listdir("."):
-		files.strip()
-		if os.path.isdir(files):
-			os.chdir(files)
-			for sub_file in os.listdir("."):
-				if os.path.isdir(sub_file):
-					os.chdir(sub_file)
-					if action == _COMPILE:
-						if not subprocess.call(["g++", "-lOpenCL", "histogram.cpp", "-o", "histogram"]):
-							# correct running
-							pass
-						else:
-							_COMPILE_ERROR_MES = files + "\n"
-							file_compile_error.write(_COMPILE_ERROR_MES)
-					elif action == _RUN:
-						# TODO: input path
-						subprocess.call(["cp", "", "input"])
-						if not subprocess.call(["./histogram"]):
-							# run correct
-							pass
-						else:
-							_RUN_ERROR_MES = files + "\n"
-							file_run_error.write(_RUN_ERROR_MES)
-					else:
-						pass
-					os.chdir("..")
-			os.chdir("..")
+    if file_compile_error is not None:
+        file_compile_error.close()
+    if file_run_error is not None:
+        file_run_error.close()
 
-	if file_compile_error is not None:
-		file_compile_error.close()
-	if file_run_error is not None:
-		file_run_error.close()
+# python runcl.py run/compile input_file
 
-# python runcl.py run/compile
+
 def main():
-	action = sys.argv[1]
-    start_action(action)
+    action = sys.argv[1]
+    input_file = sys.argv[2] if sys.argv[2] is not None else None
+    start_action(action, input_file)
 
 if __name__ == '__main__':
     main()
